cbb502d63296a418040ab48fbba860b41ec7e460
RHRolun <rlundber@redhat.com>
2025-02-19 12:27:57 +0100

    working

--- safety_checker/runtime/safety_checker/safety_checker.py
+++ safety_checker/runtime/safety_checker/safety_checker.py
@@ -1,9 +1,9 @@
 import kserve
 import numpy as np
-import logging
 import base64
 from io import BytesIO
 from typing import Dict
+import numpy as np
 
 from diffusers import StableDiffusionXLPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
@@ -12,7 +12,9 @@ import torch
 from torchvision.transforms.functional import pil_to_tensor, to_pil_image
 from PIL import Image as PIL_Image
 
-logging.basicConfig(level=logging.INFO)
+from kserve import InferResponse, InferRequest
+from kserve.logging import logger
+from kserve.utils.utils import get_predict_response
 
 class SafetyChecker(kserve.Model):
     def __init__(self, name: str):
@@ -22,33 +24,30 @@ class SafetyChecker(kserve.Model):
 
         #self.device="cuda"
         self.device="cpu"
-        dtype=torch.float16
+        self.dtype=torch.float16
 
         self.feature_extractor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
         self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
             "CompVis/stable-diffusion-safety-checker").to(self.device)
 
     def predict(self,
-        payload: Dict,
+        payload: InferRequest,
         headers: Dict[str, str] = None,
         response_headers: Dict[str, str] = None,
-    ) -> Dict:
+    ) -> InferResponse:
 
-        try:
-            image_data = inputs.get("image")
-            if not image_data:
-                return {"error": "Missing image data"}
+        logger.info(payload)
 
-            # Decode the base64 image
-            image_bytes = base64.b64decode(image_data)
-            image = PIL_Image.open(BytesIO(image_bytes)).convert("RGB")
+        image_data = payload.inputs[0].data
 
-            safety_checker_input = self.feature_extractor(image, return_tensors="pt").to(self.device)
-            image, has_nsfw_concept = self.safety_checker(
-                images=pil_to_tensor(image).unsqueeze(0), clip_input=safety_checker_input.pixel_values.to(dtype)
-            )
-            return {"predictions": has_nsfw_concept}
+        logger.info(image_data)
 
-        except Exception as e:
-            logging.error(f"Prediction failed: {e}")
-            return {"error": str(e)}
+        # Decode the base64 image
+        image_bytes = base64.b64decode(image_data[0])
+        image = PIL_Image.open(BytesIO(image_bytes)).convert("RGB")
+
+        safety_checker_input = self.feature_extractor(image, return_tensors="pt").to(self.device)
+        image, has_nsfw_concept = self.safety_checker(
+            images=pil_to_tensor(image).unsqueeze(0), clip_input=safety_checker_input.pixel_values.to(self.dtype)
+        )
+        return get_predict_response(payload, np.asarray(has_nsfw_concept), self.name)
